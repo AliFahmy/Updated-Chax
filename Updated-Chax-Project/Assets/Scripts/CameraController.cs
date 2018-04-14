176ef5e96177ca59223346784ae2918beb46f325
Ali Mahmoud <Ali.Mahmoud@cis.asu.edu.eg>
2018-04-14 14:16:39 +0200

    adjusted camera movement

---
+++ Updated-Chax-Project/Assets/Scripts/CameraController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private bool EnableMovement;
+
+    private float PanSpeed;
+    private float BorderThickness;
+    private float ScrollSpeed;
+    private float MaxY;
+    private float MinY;
+    void Awake()
+    {
+        EnableMovement = true;
+        PanSpeed = 30.0f;
+        BorderThickness = 10.0f;
+        ScrollSpeed = 5f;
+        MinY = 10f;
+        MaxY = 60f;
+    }
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            EnableMovement = !EnableMovement;
+        }
+        if (!EnableMovement)
+            return;
+        if (Input.GetKey("w") )//|| Input.mousePosition.y >= Screen.height - BorderThickness)
+        {
+            transform.Translate(Vector3.forward * PanSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey("s") )//|| Input.mousePosition.y <= Screen.height)
+        {
+            transform.Translate(Vector3.back * PanSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey("d") )//|| Input.mousePosition.x >= Screen.width - BorderThickness)
+        {
+            transform.Translate(Vector3.right * PanSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey("a") )//|| Input.mousePosition.x <= Screen.width)
+        {
+            transform.Translate(Vector3.left * PanSpeed * Time.deltaTime, Space.World);
+        }
+        float scroll=Input.GetAxis("Mouse ScrollWheel");
+        Vector3 dir = transform.position;
+        dir.y -= scroll * 1000 * ScrollSpeed * Time.deltaTime;
+        dir.y = Mathf.Clamp(dir.y, MinY, MaxY);
+        transform.position = dir;
+    }
+}
