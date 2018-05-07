164915ef037be411b6ce794b0be7d30e1d9e4d54
hazemkhairy <hazemkhairy188@gmail.com>
2018-05-07 09:20:13 +0200

    added new main menu and edited in the main menu script

--- Updated-Chax-Project/Assets/Scripts/MainMenuScript.cs
+++ Updated-Chax-Project/Assets/Scripts/MainMenuScript.cs
@@ -4,28 +4,32 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 public class MainMenuScript : MonoBehaviour {
-    public  GameObject MainMenuUI;
-    public static bool gamestarted;
-    void Awake()
-    {
-        gamestarted = false;
-    }
-    public void Update()
-    {
-        if (gamestarted == true)
-        {
-            MainMenuUI.SetActive(false);
-        }
-    }
-    public void StartGame()
-    {
-        gamestarted = true;
-        MainMenuUI.SetActive(false);
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-    }
-    public void EndGame()
-    {
-        Application.Quit();
-    }
+	public string leveltoload;
+	void Start()
+	{
+		leveltoload = "x";
+	}
+
+	public void Level1()
+	{
+		leveltoload = "FirstLevel";
+		Debug.Log (leveltoload);
+		SceneManager.LoadScene ("FirstLevel");
+		//play ();
+	}
+	public void Level2 ()
+	{
+		leveltoload = "SecondLevel";
+		play ();
+	}
+	public void Level3()
+	{
+		leveltoload = "ThirdLevel";
+		play ();
+	}
+	public void play()
+	{
+		SceneManager.LoadScene (leveltoload);
+	}
     
 }
